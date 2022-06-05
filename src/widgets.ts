0e5af0a88a9dd3cb3c9267164db6888717ff45e3
Hsiaoming Yang <me@lepture.com>
2022-06-05 17:06:55 +0900

    Add icon by default

--- src/widgets.ts
+++ src/widgets.ts
@@ -2,6 +2,10 @@ import { WidgetType, EditorView } from '@codemirror/view'
 import { ImageNode, RatioMap } from './types'
 
 export const galleryTheme = EditorView.baseTheme({
+  "@keyframes mm-uploading": {
+    from: { top: "60%" },
+    to: { top: "40%" },
+  },
   ".mm-gallery img": {
     maxWidth: "100%",
     verticalAlign: "middle",
@@ -18,10 +22,33 @@ export const galleryTheme = EditorView.baseTheme({
   ".mm-gallery-column button": {
     position: "absolute",
     padding: "0",
-    top: "8px",
-    right: "8px",
-    width: "1em",
-    height: "1em",
+    top: "10px",
+    right: "10px",
+    width: "30px",
+    height: "30px",
+    borderRadius: "50%",
+    background: "rgba(255, 255, 255, 0.5)",
+    border: "none",
+    cursor: "pointer",
+  },
+  ".mm-gallery-column svg": {
+    verticalAlign: "middle",
+  },
+  ".mm-gallery-upload": {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    width: "50px",
+    height: "50px",
+    marginTop: "-25px",
+    marginLeft: "-25px",
+    textAlign: "center",
+    animation: "mm-uploading 2s ease infinite",
+  },
+  ".mm-gallery-upload svg": {
+    width: "42px",
+    height: "42px",
+    fill: "white",
   },
 })
 
@@ -77,21 +104,17 @@ export class GalleryWidget extends WidgetType {
 
         const delButton = document.createElement('button')
         delButton.type = "button"
-        delButton.ariaLabel = "Delete"
-        const delIcon = document.createElement('i')
-        delIcon.className = "icon-delete"
-        delButton.appendChild(delIcon)
+        delButton.title = "Delete this image"
+        delButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"><path fill="none" d="M0 0h24v24H0z"/><path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z"/></svg>'
         delButton.addEventListener("click", e => {
           fireEvent(e, delButton, 'delete-image', item, this.offset)
         })
         figure.appendChild(delButton)
-        // add uploading status
+
         if (/^blob:\/\//.test(item.src)) {
           const statusDiv = document.createElement('div')
-          statusDiv.className = "mm-gallery-uploading"
-          const statusIcon = document.createElement('i')
-          statusIcon.className = "icon-upload"
-          statusDiv.appendChild(statusIcon)
+          statusDiv.className = "mm-gallery-upload"
+          statusDiv.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M13 7.828V20h-2V7.828l-5.364 5.364-1.414-1.414L12 4l7.778 7.778-1.414 1.414L13 7.828z"/></svg>'
           figure.appendChild(statusDiv)
         }
 
